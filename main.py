18c2974b4cf09109394195c7614e5e5a989ad036
matteocarta1-png <matteocarta1@gmail.com>
2026-08-17 18:20:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ PRODOTTI = [
     {"asin": "B07S7XJ9F2", "nome": "Compressore Portatile Elettrico Auto/Moto"}
 ]
 
-PERCENTUALE_MINIMA_SCONTO = 10.0  # Notifica solo con sconti >= 80%
+PERCENTUALE_MINIMA_SCONTO = 10.0  # Notifica solo con sconti >= 10%
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
